7fde4c4c9a51c7e8938bb2a16d5813d39520b246
thamiscoder <thamirisoli34@gmail.com>
2025-04-24 19:53:45 -0300

    kp

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function login() {
-    
+    let usuario = document.getElementById('logintxt');
+    let senha = document.getElementById('passtxt');
+
+    alert(`Seu nome é: ${usuario} e Sua senha é ${senha}`);
 }
