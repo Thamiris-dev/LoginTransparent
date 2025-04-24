bccf4df3b2511e3e6fecad8362fb9fe55d3a930e
thamiscoder <thamirisoli34@gmail.com>
2025-04-24 19:38:50 -0300

    dw

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
 function login() {
-    const inputs = document.querySelectorAll('.input-box input');
-    const userName = inputs[0].value;
-    const passWord = inputs[1].value;
-
-    console.log(userName);
-    console.log(passWord);
+    
 }
